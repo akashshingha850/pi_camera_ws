9bf0e0261e840530c17bae37d737a4ec29afe536
Akash Shingha <akashshingha850@gmail.com>
2025-09-02 19:53:12 +0300

    Enhance launch script to set ROS networking and IP address detection

--- launch_picam.sh
+++ launch_picam.sh
@@ -5,5 +5,15 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 # Source workspace overlay
 source "$(dirname "$0")/install/setup.bash"
 
+# Set ROS networking for multi-device access
+export ROS_LOCALHOST_ONLY=0
+IP=$(hostname -I | awk '{print $1}')
+if [ -n "$IP" ]; then
+    export ROS_IP=$IP
+    echo "ROS_IP set to $IP"
+else
+    echo "Warning: No IP address detected"
+fi
+
 # Launch the camera node (default)
 ros2 run camera_ros camera_node
